a94ed8584315aa29ccf5011807f2b5b9824a87cc
Darius19920110 <meszidani22@gmail.com>
2022-07-07 03:32:44 +0100

    Creating user choice function

--- manual_rps.py
+++ manual_rps.py
@@ -15,3 +15,24 @@ def get_computer_choice():
 
     # return the name of the choice
     return choices[randnum]
+
+def get_user_choice():
+    # Ask for user input, q for exit the program. If unknown word or number was added, start over.
+    while True:
+        try:
+            choice = input("Your choice is: ")
+
+            if choice.lower() == "rock":
+                return "rock"
+            elif choice.lower() == "paper":
+                return "paper"
+            elif choice.lower() == "scissors":
+                return "scissors"
+            elif choice.lower() == "q":
+                return "exit"
+            else:
+                print("Invalid choice!")
+                continue
+        except:
+            print("Invalid choice!")
+            continue
